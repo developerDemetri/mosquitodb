a49006d418d0b8b0f47c0d769c3912cc2a207af7
DeveloperDemetri <developerdemetri@gmail.com>
2016-10-09 12:59:45 -0700

    successfully parsing wikipedia list of all 3200 or so US counties

--- bin/county_parser.js
+++ bin/county_parser.js
@@ -1,16 +1,34 @@
 'use strict';
 
-let html_parser = require('htmlparser2');
+let fs = require('fs');
+let cheerio = require('cheerio');
+
+function isEmpty(str) {
+  if (str.trim() === '') {
+    return false;
+  }
+  return true;
+}
 
 function crawl() {
   let counties = [];
-  // crawl wikipedia page //
-  let maricopa = {
-    name: 'Maricopa County',
-    state: 'Arizona'
-  };
-  counties.push(maricopa);//TODO: just for testing //
-
+  console.log('Parsing wikipedia page...');
+  let wiki_html = fs.readFileSync('bin/us_counties_wikipedia.html').toString();
+  let $ = cheerio.load(wiki_html);
+  let raw_list = $('body').find('ol').text();
+  raw_list = raw_list.substring(0,raw_list.indexOf('^')).split("\n").filter(isEmpty);
+  for (let i = 0; i < raw_list.length; i++) {
+    let county_parts = raw_list[i].split(',');
+    if (county_parts[1].indexOf('[') > 0) {
+      county_parts[1] = county_parts[1].split('[')[0];
+    }
+    let county = {
+      name: county_parts[0].trim(),
+      state: county_parts[1].trim()
+    };
+    counties.push(county);
+  }
+  console.log('Done parsing wikipedia page.');
   return counties;
 }
 

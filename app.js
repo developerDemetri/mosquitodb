828968fd5cd7db8da171e200333c610882a068d9
DeveloperDemetri <developerdemetri@gmail.com>
2016-10-09 14:20:41 -0700

    setting up data submit page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let redis_tool = require('./bin/redis_tool');
 let session_tool = require('./bin/session_tool');
 
 let index = require('./routes/index');
+let submit = require('./routes/submit');
 
 let app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static('./public/javascripts'));
 app.use(session_tool);
 
 app.use('/', index);
+app.use('/submit', submit);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');

94667b22a71c4cb5acbca27a4af39ca8fd5d84c0
DeveloperDemetri <developerdemetri@gmail.com>
2016-11-13 16:36:55 -0700

    setting up query page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,72 @@
 'use strict';
 
-angular.module('mosquitoApp').controller('homeController', function ($scope, $http) {
-  $scope.greeting = 'Coming Soon';
+angular.module('mosquitoApp').controller('homeController', function ($scope, $http, $timeout) {
+
+  $scope.states = [];
+  $scope.counties = [];
+  $scope.startYear = 1900;
+  $scope.endYear = 2017;
+  $scope.state = null;
+  $scope.county = null;
+  $scope.spec = null;
+
+  function setup() {
+
+  }
+
+  $scope.search = function() {
+    $('#search-results').removeClass('hidden');
+  }
+
+  function backToOptions() {
+    $scope.searching = true;
+  }
+
+  $http.get(getServer()+'/states').then(function(response) {
+    if (response.data.status === 200) {
+      $('#state').selectpicker();
+      $scope.states = response.data.states;
+      $timeout(function() {
+        $('#state').selectpicker('refresh');
+      }, 1);
+    }
+    else {
+      console.log(response.data.error);
+    }
+  });
+
+  $http.get(getServer()+'/species').then(function(response) {
+    if (response.data.status === 200) {
+      $('#species').selectpicker();
+      $scope.species = response.data.species;
+      $timeout(function() {
+        $('#species').selectpicker('refresh');
+      }, 1);
+    }
+    else {
+      console.log(response.data.error);
+    }
+  });
+
+  function populateCounties() {
+    $http.get(getServer()+'/counties/'+$scope.state).then(function(response) {
+      if (response.data.status === 200) {
+        $('#county').selectpicker();
+        $scope.counties = response.data.counties;
+        $timeout(function() {
+          $('#county').selectpicker('refresh');
+        }, 1);
+      }
+      else {
+        console.log(response.data.error);
+      }
+    });
+  }
+
+  $scope.$watch('state', function(newvalue, oldvalue) {
+    if (newvalue) {
+      populateCounties();
+    }
+  });
+
 });

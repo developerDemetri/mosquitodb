29563763dfcd1df27047ab20bebd04427d0ca69b
DeveloperDemetri <developerdemetri@gmail.com>
2017-01-24 23:13:31 -0700

    added redis db setting

--- bin/secret_settings.js
+++ bin/secret_settings.js
@@ -13,6 +13,7 @@ let sesh_secret;
 let redis_port;
 let redis_host;
 let redis_password;
+let redis_db;
 
 let mdb_api_key;
 let captcha_secret;
@@ -28,11 +29,17 @@ if (process.env.im_live) {
   pg_port = pg_url[3].split('/')[0];
   sesh_name = process.env.sesh_name;
   sesh_secret = process.env.sesh_secret;
-  pg_ssl = true;
+  pg_ssl = process.env.pg_ssl;
   let redis_url = process.env.REDIS_URL.split(':');
   redis_port = redis_url[3];
   redis_host = redis_url[2].split('@')[1];
   redis_password = redis_url[2].split('@')[0];
+  if (process.env.redis_db) {
+    redis_db = process.env.redis_db;
+  }
+  else {
+    redis_db = 0;
+  }
   mdb_api_key = process.env.mdb_api_key;
   captcha_secret = process.env.captcha_secret;
 }
@@ -46,10 +53,11 @@ else {
   pg_port = local_settings.pg_port;
   sesh_name = local_settings.sesh_name;
   sesh_secret = local_settings.sesh_secret;
-  pg_ssl = false;
+  pg_ssl = local_settings.pg_ssl;
   redis_port = local_settings.redis_port;
   redis_host = local_settings.redis_host;
   redis_password = local_settings.redis_password;
+  redis_db = local_settings.redis_db;
   mdb_api_key = local_settings.mdb_api_key;
   captcha_secret = local_settings.captcha_secret;
 }
@@ -77,7 +85,8 @@ const api_settings = {
 const redis_config = {
   port: redis_port,
   host: redis_host,
-  password: redis_password
+  password: redis_password,
+  db: redis_db
 };
 
 const secret_settings = {
